c3ac85799efdd957e4b90a282f68460809967947
Wizard-555 <kishankishore55555@gmail.com>
2025-12-28 18:14:21 +0530

    creating and implementing our first component (MyFun)

---
+++ first_basic_project/src/App.jsx
@@ -0,0 +1,17 @@
+import MyFun from "./myOwn"
+
+function App() {
+
+  return (
+    // <h1>My name is Kishan</h1>
+
+    // this is called Fragments  (<> </>)
+    <>       
+    <MyFun/>
+    <h2>my second line</h2>
+    <p>First para</p>
+    </>
+  )
+}
+
+export default App
---
+++ first_basic_project/src/myOwn.jsx
@@ -0,0 +1,7 @@
+function MyFun(){
+    return (
+       <h3>This is my own JSX  file</h3> 
+    )
+}
+
+export default MyFun
